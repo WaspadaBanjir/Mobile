e2a120f6852b112029d3c0eefcd31bbd4ae32ac5
Andrea <lk.andrea@ymail.com>
2017-12-08 12:41:50 +0700

    License

--- app/src/main/java/now/koder/waspadabanjir/MainActivity.java
+++ app/src/main/java/now/koder/waspadabanjir/MainActivity.java
@@ -1,10 +1,14 @@
 package now.koder.waspadabanjir;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,8 +22,15 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,29 @@ public class MainActivity extends AppCompatActivity
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        webview.loadUrl("http://10.21.19.156/Waspada-Banjir/public/login");
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
+
+        webview.loadUrl("http://siaab.andrewij.com/public/login");
     }
 
     @Override
@@ -93,13 +126,15 @@ public class MainActivity extends AppCompatActivity
 
 
         if (id == R.id.nav_login) {
-            webview.loadUrl("http://10.21.19.156/Waspada-Banjir/public/login");
+            webview.loadUrl("http:/siaab.andrewij.com/public/login");
         } else if (id == R.id.nav_register) {
-            webview.loadUrl("http://10.21.19.156/Waspada-Banjir/public/register");
+            webview.loadUrl("http://siaab.andrewij.com/public/register");
         } else if (id == R.id.nav_report) {
-            webview.loadUrl("http://10.21.19.156/Waspada-Banjir/public/home");
+            webview.loadUrl("http://siaab.andrewij.com/public/home");
         } else if (id == R.id.nav_manage) {
-            webview.loadUrl("http://10.21.19.156/Waspada-Banjir/public/pinpoint");
+            webview.loadUrl("http://siaab.andrewij.com/public/pinpoint");
+        } else if (id == R.id.nav_license) {
+            webview.loadUrl("http://siaab.andrewij.com/public/license");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -107,3 +142,4 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
+
